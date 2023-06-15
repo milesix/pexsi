b2073ae9c26b23ebde9cadc52221dcd29c2165b9
Lin Lin <linlin@math.berkeley.edu>
2023-06-14 22:46:47 -0700

    add SuperLU_DIST_8.1.0 and parmetis_4.0.3 to external
    These dependencies should be shipped with PEXSI

---
+++ external/parmetis_4.0.3/metis/build/Linux-x86_64/libmetis/CMakeFiles/metis.dir/DependInfo.cmake
@@ -0,0 +1,83 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/linlin/Software/parmetis-4.0.3/metis/GKlib/b64.c" "/home/linlin/Software/parmetis-4.0.3/metis/build/Linux-x86_64/libmetis/CMakeFiles/metis.dir/__/GKlib/b64.c.o"
+  "/home/linlin/Software/parmetis-4.0.3/metis/GKlib/blas.c" "/home/linlin/Software/parmetis-4.0.3/metis/build/Linux-x86_64/libmetis/CMakeFiles/metis.dir/__/GKlib/blas.c.o"
+  "/home/linlin/Software/parmetis-4.0.3/metis/GKlib/csr.c" "/home/linlin/Software/parmetis-4.0.3/metis/build/Linux-x86_64/libmetis/CMakeFiles/metis.dir/__/GKlib/csr.c.o"
+  "/home/linlin/Software/parmetis-4.0.3/metis/GKlib/error.c" "/home/linlin/Software/parmetis-4.0.3/metis/build/Linux-x86_64/libmetis/CMakeFiles/metis.dir/__/GKlib/error.c.o"
+  "/home/linlin/Software/parmetis-4.0.3/metis/GKlib/evaluate.c" "/home/linlin/Software/parmetis-4.0.3/metis/build/Linux-x86_64/libmetis/CMakeFiles/metis.dir/__/GKlib/evaluate.c.o"
+  "/home/linlin/Software/parmetis-4.0.3/metis/GKlib/fkvkselect.c" "/home/linlin/Software/parmetis-4.0.3/metis/build/Linux-x86_64/libmetis/CMakeFiles/metis.dir/__/GKlib/fkvkselect.c.o"
+  "/home/linlin/Software/parmetis-4.0.3/metis/GKlib/fs.c" "/home/linlin/Software/parmetis-4.0.3/metis/build/Linux-x86_64/libmetis/CMakeFiles/metis.dir/__/GKlib/fs.c.o"
+  "/home/linlin/Software/parmetis-4.0.3/metis/GKlib/getopt.c" "/home/linlin/Software/parmetis-4.0.3/metis/build/Linux-x86_64/libmetis/CMakeFiles/metis.dir/__/GKlib/getopt.c.o"
+  "/home/linlin/Software/parmetis-4.0.3/metis/GKlib/gkregex.c" "/home/linlin/Software/parmetis-4.0.3/metis/build/Linux-x86_64/libmetis/CMakeFiles/metis.dir/__/GKlib/gkregex.c.o"
+  "/home/linlin/Software/parmetis-4.0.3/metis/GKlib/graph.c" "/home/linlin/Software/parmetis-4.0.3/metis/build/Linux-x86_64/libmetis/CMakeFiles/metis.dir/__/GKlib/graph.c.o"
+  "/home/linlin/Software/parmetis-4.0.3/metis/GKlib/htable.c" "/home/linlin/Software/parmetis-4.0.3/metis/build/Linux-x86_64/libmetis/CMakeFiles/metis.dir/__/GKlib/htable.c.o"
+  "/home/linlin/Software/parmetis-4.0.3/metis/GKlib/io.c" "/home/linlin/Software/parmetis-4.0.3/metis/build/Linux-x86_64/libmetis/CMakeFiles/metis.dir/__/GKlib/io.c.o"
+  "/home/linlin/Software/parmetis-4.0.3/metis/GKlib/itemsets.c" "/home/linlin/Software/parmetis-4.0.3/metis/build/Linux-x86_64/libmetis/CMakeFiles/metis.dir/__/GKlib/itemsets.c.o"
+  "/home/linlin/Software/parmetis-4.0.3/metis/GKlib/mcore.c" "/home/linlin/Software/parmetis-4.0.3/metis/build/Linux-x86_64/libmetis/CMakeFiles/metis.dir/__/GKlib/mcore.c.o"
+  "/home/linlin/Software/parmetis-4.0.3/metis/GKlib/memory.c" "/home/linlin/Software/parmetis-4.0.3/metis/build/Linux-x86_64/libmetis/CMakeFiles/metis.dir/__/GKlib/memory.c.o"
+  "/home/linlin/Software/parmetis-4.0.3/metis/GKlib/omp.c" "/home/linlin/Software/parmetis-4.0.3/metis/build/Linux-x86_64/libmetis/CMakeFiles/metis.dir/__/GKlib/omp.c.o"
+  "/home/linlin/Software/parmetis-4.0.3/metis/GKlib/pdb.c" "/home/linlin/Software/parmetis-4.0.3/metis/build/Linux-x86_64/libmetis/CMakeFiles/metis.dir/__/GKlib/pdb.c.o"
+  "/home/linlin/Software/parmetis-4.0.3/metis/GKlib/pqueue.c" "/home/linlin/Software/parmetis-4.0.3/metis/build/Linux-x86_64/libmetis/CMakeFiles/metis.dir/__/GKlib/pqueue.c.o"
+  "/home/linlin/Software/parmetis-4.0.3/metis/GKlib/random.c" "/home/linlin/Software/parmetis-4.0.3/metis/build/Linux-x86_64/libmetis/CMakeFiles/metis.dir/__/GKlib/random.c.o"
+  "/home/linlin/Software/parmetis-4.0.3/metis/GKlib/rw.c" "/home/linlin/Software/parmetis-4.0.3/metis/build/Linux-x86_64/libmetis/CMakeFiles/metis.dir/__/GKlib/rw.c.o"
+  "/home/linlin/Software/parmetis-4.0.3/metis/GKlib/seq.c" "/home/linlin/Software/parmetis-4.0.3/metis/build/Linux-x86_64/libmetis/CMakeFiles/metis.dir/__/GKlib/seq.c.o"
+  "/home/linlin/Software/parmetis-4.0.3/metis/GKlib/sort.c" "/home/linlin/Software/parmetis-4.0.3/metis/build/Linux-x86_64/libmetis/CMakeFiles/metis.dir/__/GKlib/sort.c.o"
+  "/home/linlin/Software/parmetis-4.0.3/metis/GKlib/string.c" "/home/linlin/Software/parmetis-4.0.3/metis/build/Linux-x86_64/libmetis/CMakeFiles/metis.dir/__/GKlib/string.c.o"
+  "/home/linlin/Software/parmetis-4.0.3/metis/GKlib/timers.c" "/home/linlin/Software/parmetis-4.0.3/metis/build/Linux-x86_64/libmetis/CMakeFiles/metis.dir/__/GKlib/timers.c.o"
+  "/home/linlin/Software/parmetis-4.0.3/metis/GKlib/tokenizer.c" "/home/linlin/Software/parmetis-4.0.3/metis/build/Linux-x86_64/libmetis/CMakeFiles/metis.dir/__/GKlib/tokenizer.c.o"
+  "/home/linlin/Software/parmetis-4.0.3/metis/GKlib/util.c" "/home/linlin/Software/parmetis-4.0.3/metis/build/Linux-x86_64/libmetis/CMakeFiles/metis.dir/__/GKlib/util.c.o"
+  "/home/linlin/Software/parmetis-4.0.3/metis/libmetis/auxapi.c" "/home/linlin/Software/parmetis-4.0.3/metis/build/Linux-x86_64/libmetis/CMakeFiles/metis.dir/auxapi.c.o"
+  "/home/linlin/Software/parmetis-4.0.3/metis/libmetis/balance.c" "/home/linlin/Software/parmetis-4.0.3/metis/build/Linux-x86_64/libmetis/CMakeFiles/metis.dir/balance.c.o"
+  "/home/linlin/Software/parmetis-4.0.3/metis/libmetis/bucketsort.c" "/home/linlin/Software/parmetis-4.0.3/metis/build/Linux-x86_64/libmetis/CMakeFiles/metis.dir/bucketsort.c.o"
+  "/home/linlin/Software/parmetis-4.0.3/metis/libmetis/checkgraph.c" "/home/linlin/Software/parmetis-4.0.3/metis/build/Linux-x86_64/libmetis/CMakeFiles/metis.dir/checkgraph.c.o"
+  "/home/linlin/Software/parmetis-4.0.3/metis/libmetis/coarsen.c" "/home/linlin/Software/parmetis-4.0.3/metis/build/Linux-x86_64/libmetis/CMakeFiles/metis.dir/coarsen.c.o"
+  "/home/linlin/Software/parmetis-4.0.3/metis/libmetis/compress.c" "/home/linlin/Software/parmetis-4.0.3/metis/build/Linux-x86_64/libmetis/CMakeFiles/metis.dir/compress.c.o"
+  "/home/linlin/Software/parmetis-4.0.3/metis/libmetis/contig.c" "/home/linlin/Software/parmetis-4.0.3/metis/build/Linux-x86_64/libmetis/CMakeFiles/metis.dir/contig.c.o"
+  "/home/linlin/Software/parmetis-4.0.3/metis/libmetis/debug.c" "/home/linlin/Software/parmetis-4.0.3/metis/build/Linux-x86_64/libmetis/CMakeFiles/metis.dir/debug.c.o"
+  "/home/linlin/Software/parmetis-4.0.3/metis/libmetis/fm.c" "/home/linlin/Software/parmetis-4.0.3/metis/build/Linux-x86_64/libmetis/CMakeFiles/metis.dir/fm.c.o"
+  "/home/linlin/Software/parmetis-4.0.3/metis/libmetis/fortran.c" "/home/linlin/Software/parmetis-4.0.3/metis/build/Linux-x86_64/libmetis/CMakeFiles/metis.dir/fortran.c.o"
+  "/home/linlin/Software/parmetis-4.0.3/metis/libmetis/frename.c" "/home/linlin/Software/parmetis-4.0.3/metis/build/Linux-x86_64/libmetis/CMakeFiles/metis.dir/frename.c.o"
+  "/home/linlin/Software/parmetis-4.0.3/metis/libmetis/gklib.c" "/home/linlin/Software/parmetis-4.0.3/metis/build/Linux-x86_64/libmetis/CMakeFiles/metis.dir/gklib.c.o"
+  "/home/linlin/Software/parmetis-4.0.3/metis/libmetis/graph.c" "/home/linlin/Software/parmetis-4.0.3/metis/build/Linux-x86_64/libmetis/CMakeFiles/metis.dir/graph.c.o"
+  "/home/linlin/Software/parmetis-4.0.3/metis/libmetis/initpart.c" "/home/linlin/Software/parmetis-4.0.3/metis/build/Linux-x86_64/libmetis/CMakeFiles/metis.dir/initpart.c.o"
+  "/home/linlin/Software/parmetis-4.0.3/metis/libmetis/kmetis.c" "/home/linlin/Software/parmetis-4.0.3/metis/build/Linux-x86_64/libmetis/CMakeFiles/metis.dir/kmetis.c.o"
+  "/home/linlin/Software/parmetis-4.0.3/metis/libmetis/kwayfm.c" "/home/linlin/Software/parmetis-4.0.3/metis/build/Linux-x86_64/libmetis/CMakeFiles/metis.dir/kwayfm.c.o"
+  "/home/linlin/Software/parmetis-4.0.3/metis/libmetis/kwayrefine.c" "/home/linlin/Software/parmetis-4.0.3/metis/build/Linux-x86_64/libmetis/CMakeFiles/metis.dir/kwayrefine.c.o"
+  "/home/linlin/Software/parmetis-4.0.3/metis/libmetis/mcutil.c" "/home/linlin/Software/parmetis-4.0.3/metis/build/Linux-x86_64/libmetis/CMakeFiles/metis.dir/mcutil.c.o"
+  "/home/linlin/Software/parmetis-4.0.3/metis/libmetis/mesh.c" "/home/linlin/Software/parmetis-4.0.3/metis/build/Linux-x86_64/libmetis/CMakeFiles/metis.dir/mesh.c.o"
+  "/home/linlin/Software/parmetis-4.0.3/metis/libmetis/meshpart.c" "/home/linlin/Software/parmetis-4.0.3/metis/build/Linux-x86_64/libmetis/CMakeFiles/metis.dir/meshpart.c.o"
+  "/home/linlin/Software/parmetis-4.0.3/metis/libmetis/minconn.c" "/home/linlin/Software/parmetis-4.0.3/metis/build/Linux-x86_64/libmetis/CMakeFiles/metis.dir/minconn.c.o"
+  "/home/linlin/Software/parmetis-4.0.3/metis/libmetis/mincover.c" "/home/linlin/Software/parmetis-4.0.3/metis/build/Linux-x86_64/libmetis/CMakeFiles/metis.dir/mincover.c.o"
+  "/home/linlin/Software/parmetis-4.0.3/metis/libmetis/mmd.c" "/home/linlin/Software/parmetis-4.0.3/metis/build/Linux-x86_64/libmetis/CMakeFiles/metis.dir/mmd.c.o"
+  "/home/linlin/Software/parmetis-4.0.3/metis/libmetis/ometis.c" "/home/linlin/Software/parmetis-4.0.3/metis/build/Linux-x86_64/libmetis/CMakeFiles/metis.dir/ometis.c.o"
+  "/home/linlin/Software/parmetis-4.0.3/metis/libmetis/options.c" "/home/linlin/Software/parmetis-4.0.3/metis/build/Linux-x86_64/libmetis/CMakeFiles/metis.dir/options.c.o"
+  "/home/linlin/Software/parmetis-4.0.3/metis/libmetis/parmetis.c" "/home/linlin/Software/parmetis-4.0.3/metis/build/Linux-x86_64/libmetis/CMakeFiles/metis.dir/parmetis.c.o"
+  "/home/linlin/Software/parmetis-4.0.3/metis/libmetis/pmetis.c" "/home/linlin/Software/parmetis-4.0.3/metis/build/Linux-x86_64/libmetis/CMakeFiles/metis.dir/pmetis.c.o"
+  "/home/linlin/Software/parmetis-4.0.3/metis/libmetis/refine.c" "/home/linlin/Software/parmetis-4.0.3/metis/build/Linux-x86_64/libmetis/CMakeFiles/metis.dir/refine.c.o"
+  "/home/linlin/Software/parmetis-4.0.3/metis/libmetis/separator.c" "/home/linlin/Software/parmetis-4.0.3/metis/build/Linux-x86_64/libmetis/CMakeFiles/metis.dir/separator.c.o"
+  "/home/linlin/Software/parmetis-4.0.3/metis/libmetis/sfm.c" "/home/linlin/Software/parmetis-4.0.3/metis/build/Linux-x86_64/libmetis/CMakeFiles/metis.dir/sfm.c.o"
+  "/home/linlin/Software/parmetis-4.0.3/metis/libmetis/srefine.c" "/home/linlin/Software/parmetis-4.0.3/metis/build/Linux-x86_64/libmetis/CMakeFiles/metis.dir/srefine.c.o"
+  "/home/linlin/Software/parmetis-4.0.3/metis/libmetis/stat.c" "/home/linlin/Software/parmetis-4.0.3/metis/build/Linux-x86_64/libmetis/CMakeFiles/metis.dir/stat.c.o"
+  "/home/linlin/Software/parmetis-4.0.3/metis/libmetis/timing.c" "/home/linlin/Software/parmetis-4.0.3/metis/build/Linux-x86_64/libmetis/CMakeFiles/metis.dir/timing.c.o"
+  "/home/linlin/Software/parmetis-4.0.3/metis/libmetis/util.c" "/home/linlin/Software/parmetis-4.0.3/metis/build/Linux-x86_64/libmetis/CMakeFiles/metis.dir/util.c.o"
+  "/home/linlin/Software/parmetis-4.0.3/metis/libmetis/wspace.c" "/home/linlin/Software/parmetis-4.0.3/metis/build/Linux-x86_64/libmetis/CMakeFiles/metis.dir/wspace.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../../GKlib"
+  "../../include"
+  "../../libmetis/."
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
