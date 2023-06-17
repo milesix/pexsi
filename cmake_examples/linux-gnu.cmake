b4df11afc2fbbb5d365a02aa430fdc0cca3a52c4
wanghongyu <1354998814@qq.com>
2023-06-17 14:44:36 +0800

    OpOimized OpenMP options

--- cmake_examples/linux-gnu.cmake
+++ cmake_examples/linux-gnu.cmake
@@ -4,7 +4,6 @@
 set( CMAKE_C_COMPILER       mpicc    )
 set( CMAKE_CXX_COMPILER     mpicxx   )
 set( CMAKE_Fortran_COMPILER mpif90   )
-set(PEXSI_ENABLE_OPENMP       ON)
 
 set( BLAS_PREFIX            "/share/home/wanghongyu/spack/opt/spack/linux-centos7-haswell/gcc-4.8.5/openblas-0.3.21-25je2lbzvjfhz5aujeakwbd3jz2ldog4/" )
 set( LAPACK_PREFIX          "${BLAS_PREFIX}" ) # OpenBLAS has a LAPACK linker
